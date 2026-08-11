2ee99a5b94dae6e895a20ca939b1e7e70e7d556c
venkataramanakolleti <venkataramanakolleti593@gmail.com>
2026-08-11 12:00:57 +0530

    Update database connection

--- db_connection.py
+++ db_connection.py
@@ -5,7 +5,7 @@ def create_connection():
     connection=mysql.connector.connect(
         host='localhost',
         user='root',
-        password='YOUR_MYSQL-PASSWORD',
+        password='YOUR_MYSQL_PASSWORD',
         database='green_bloom_db' 
     )
     return connection
